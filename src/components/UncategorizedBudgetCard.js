d9a46bd0ac5076fff7f34793d8adcf1d43987dc0
Gabriel Torres <gabrieltorresbolognai@gmail.com>
2022-05-29 15:52:02 -0300

    refactor: primitive obsession from UncategorizedBudgetCard removed.

--- src/components/UncategorizedBudgetCard.js
+++ src/components/UncategorizedBudgetCard.js
@@ -1,11 +1,13 @@
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "../contexts/BudgetsContext"
 import BudgetCard from "./BudgetCard"
 
+export const BUDGET_REDUCE_NUMBER = 0
+
 export default function UncategorizedBudgetCard(props) {
   const { getBudgetExpenses } = useBudgets()
   const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce(
     (total, expense) => total + expense.amount,
-    0
+    BUDGET_REDUCE_NUMBER
   )
   if (amount === 0) return null
 
