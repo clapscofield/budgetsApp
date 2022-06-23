2857f1dee5480ac4afcb538936eece426d240fae
goncalves-ac <goncalves.ac@outlook.com>
2022-06-23 10:57:27 -0300

    Add file unit test

---
+++ src/tests/App.unit.test.js
@@ -0,0 +1,12 @@
+//in e2e/tests/autocomplete.js
+const webdriver = require("selenium-webdriver");
+const script = require('jest');
+const { By, until } = webdriver; 
+const driver = new webdriver.Builder().forBrowser("firefox").build();
+const url = 'http://localhost:3000/'
+
+describe("add budget form", () => {
+
+    
+
+});
